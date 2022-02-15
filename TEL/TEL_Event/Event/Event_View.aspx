97d2cd13f02056348f6656773ea89caedb1fcb96
chh26 <happiness26_26@hotmail.com>
2022-02-15 22:46:19 +0800

    20220215_Shinn_活動檢視頁面

---
+++ TEL/TEL_Event/Event/Event_View.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Event_View.aspx.cs" Inherits="Event_Event_View" %>
+
+<%@ Register Src="~/Event/UserControl/UC_EventDescription.ascx" TagPrefix="uc1" TagName="UC_EventDescription" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <uc1:UC_EventDescription runat="server" ID="UC_EventDescription" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TEL/TEL_Event/Event/Event_View.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Event_Event_View : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string id = Request.QueryString["id"];
+            if (!string.IsNullOrEmpty(id))
+            {
+                this.UC_EventDescription.setViewDefault(id);
+            }
+            else
+            {
+                //沒有id的頁面，都回到導頁的原始頁
+            }
+        }
+    }
+}
